9f216007d178d92bbf3ea639b375bc20a41190f0
Jamel Achahbar <jachahbar@microsoft.com>
2023-08-01 17:30:07 +0200

    second commit

--- client/src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:3001";
-
-export const registerUser = (userData) =>
-  axios.post("/auth/register", userData);
-export const loginUser = (credentials) =>
-  axios.post("/auth/login", credentials);
-// Add other API calls as needed
